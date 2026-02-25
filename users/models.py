cdd4bf545ccbf0edcf19e7375f986c7c2866398c
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2026-02-25 12:43:32 +0100

    fix  the super user

--- users/models.py
+++ users/models.py
@@ -1,14 +1,40 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from core.models import HomeMarketBase
 from global_data.enum import UserType
 from django.utils.translation import gettext_lazy as _
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('user_type', UserType.ADMIN)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
 class User(AbstractUser, HomeMarketBase):
+    username = None
     email = models.EmailField(_('email address'), unique=True)
     user_type = models.CharField(max_length=10, choices=UserType.choices, default=UserType.BUYER)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
+    
+    objects = UserManager()
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = [] # Only email and password required for superuser
